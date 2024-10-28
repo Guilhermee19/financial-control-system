82cec78bc3c8a0bc50fc9ad029c81b3deb5f7b6c
Guilhermee19 <guilherme@noclaftech.com>
2024-10-27 23:38:42 -0300

    chore(env): update enviroment versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   base_url: 'https://finance-api.iamgui.dev/',
   socket_url: 'ws://5.252.54.146:8000/',
-  version: '5.0.2',
+  version: '5.0.3',
   firebase: {
     apiKey: 'AIzaSyB7rAy4D1JSMPNeo9GeDkbUGxNKDCfU9Bs',
     authDomain: 'financial-control-system-882ab.firebaseapp.com',
